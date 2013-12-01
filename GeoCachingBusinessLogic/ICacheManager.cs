9f78727d9fbb09b9296046ed3eaf11d7418b6d3d
TheBunksTie <TheBunksTie@gmx.net>
2013-12-01 11:20:06 +0100

    added basic interfaces for business logic

---
+++ GeoCachingBusinessLogic/ICacheManager.cs
@@ -0,0 +1,9 @@
+﻿using Swk5.GeoCaching.DomainModel;
+
+namespace Swk5.GeoCaching.BusinessLogic {
+    public interface ICacheManager {
+        bool CreateCache(Cache c);
+        bool EditCache(Cache c);
+        bool DeleteCache(Cache c);
+    }
+}
---
+++ GeoCachingBusinessLogic/IUserManager.cs
@@ -0,0 +1,9 @@
+﻿using Swk5.GeoCaching.DomainModel;
+
+namespace Swk5.GeoCaching.BusinessLogic {
+    public interface IUserAdmin {
+        bool CreateUser(User u);
+        bool EditUser(User u);
+        bool DeleteUser(User u);
+    }
+}
