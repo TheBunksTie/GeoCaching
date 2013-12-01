d23df9c920be11ca4871499c2a30c09414b93796
TheBunksTie <TheBunksTie@gmx.net>
2013-12-01 11:08:40 +0100

    added basic structure of WPF client including MainWindow and UserControl für user management

---
+++ GeoCachingDesktopClient/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Swk5.GeoCaching.Desktop {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ GeoCachingDesktopClient/UserEditor.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Swk5.GeoCaching.Desktop {
+    /// <summary>
+    /// Interaction logic for UserEditor.xaml
+    /// </summary>
+    public partial class UserEditor : UserControl {
+        public UserEditor ( ) {
+            InitializeComponent();
+        }
+    }
+}
