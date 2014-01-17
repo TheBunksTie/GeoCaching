18fd881ad0f2ee0a0c7e741d3c4258a354c9f356
TheBunksTie <TheBunksTie@gmx.net>
2014-01-17 20:30:33 +0100

    attached source for earlier commit CacheDetails for java-side viewing of all asigned details CacheFilter for easy access on subsets of all caches

---
+++ DomainModel/CacheDetails.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Swk5.GeoCaching.DomainModel {
+    public class CacheDetails {
+        public Cache Cache { get; set; }
+        public List<Image> Images { get; set; }
+        public double Rating { get; set; }
+        public List<LogEntry> LogEntries { get; set; }
+    }
+}
---
+++ DomainModel/CacheFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Swk5.GeoCaching.DomainModel {
+    public class CacheFilter {
+        public GeoPosition FromPosition { get; set; }
+        public GeoPosition ToPosition { get; set; }
+        
+        public DateTime FromCreationDate { get; set; }
+        public DateTime ToCreationDate { get; set; }
+
+        public int FromCacheSize { get; set; }
+        public int ToCacheSize { get; set; }
+
+        public double FromCacheDifficulty { get; set; }
+        public double ToCacheDifficulty { get; set; }
+
+        public double FromTerrainDifficulty { get; set; }
+        public double ToTerrainDifficulty { get; set; }
+
+    }
+}
